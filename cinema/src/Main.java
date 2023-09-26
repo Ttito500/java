c1f93c182395c38b7b91f9c0f1a9e33d69caca85
Tiago <tiago.tito.sampaio@gmail.com>
2023-09-26 19:15:02 -0300

    comesei cinema

---
+++ cinema/src/Main.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+class Client {
+    private String id;
+    private String fone;
+    public Client(String id, String fone) {
+        this.id = id;
+        this.fone = fone;
+    }
+
+    @Override
+    public String toString() {
+        return id + ":" + fone;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFone() {
+        return this.fone;
+    }
+
+    public void setFone(String fone) {
+
+    }
+}
+
+
+class Sala{
+    private List<Client> cadeiras;
+
+
+    public Sala(int capacidade) {
+        cadeiras = new ArrayList<>(); // Initialize the cadeiras list
+        for (int i = 0; i < capacidade; i++) {
+            cadeiras.add(null);
+        }
+    }
+
+
+    public List<Client> getCadeiras() {
+        return null;
+    }
+
+    public boolean reservar(String id, String fone, int ind) {
+        if (cadeiras.get(ind) == null){
+            Client set = new Client(id, fone);
+            cadeiras.set(ind, set);
+            return false;
+        }else {
+            System.out.println("fail: cadeira ja esta ocupada");
+            return false;
+        }
+    }
+
+    public void cancelar(String id) {
+    }
+
+    @Override
+    public String toString() {
+        String saida = "[ ";
+        for (Client cliente : cadeiras) {
+            if(cliente == null)
+                saida += "- ";
+            else
+                saida += cliente + " ";
+        }
+        return saida + "]";
+    }
+}
+
+class Solver {
+    static Shell sh = new Shell();
+    static Sala sala = new Sala(0);
+
+    public static void main(String args[]) {
+        sh.chain.put("init",     () -> { sala = new Sala(getInt(1));});
+        sh.chain.put("show",     () -> { System.out.println(sala);});
+        sh.chain.put("reservar", () -> { sala.reservar(getStr(1), getStr(2), getInt(3));});
+        sh.chain.put("cancelar", () -> { sala.cancelar(getStr(1));});
+
+        sh.execute();
+    }
+
+    static int getInt(int pos) {
+        return Integer.parseInt(sh.param.get(pos));
+    }
+    static String getStr(int pos) {
+        return sh.param.get(pos);
+    }
+}
+
+class Shell {
+    public Scanner scanner = new Scanner(System.in);
+    public HashMap<String, Runnable> chain = new HashMap<>();
+    public ArrayList<String> param = new ArrayList<>();
+    public Shell() {
+        Locale.setDefault(new Locale("en", "US"));
+    }
+    public void execute() {
+        while(true) {
+            param.clear();
+            String line = scanner.nextLine();
+            Collections.addAll(param, line.split(" "));
+            System.out.println("$" + line);
+            if(param.get(0).equals("end")) {
+                break;
+            } else if (chain.containsKey(param.get(0))) {
+                chain.get(param.get(0)).run();
+            } else {
+                System.out.println("fail: comando invalido");
+            }
+        }
+    }
+}
